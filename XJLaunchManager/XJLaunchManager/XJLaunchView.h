066535b1654d05adbcff73cd5b9ff4541acebfef
XJIMI <fn5128@gmail.com>
2018-12-09 15:35:27 +0800

    remove dependency

--- XJLaunchManager/XJLaunchManager/XJLaunchView.h
+++ XJLaunchManager/XJLaunchManager/XJLaunchView.h
@@ -7,10 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <Masonry/Masonry.h>
 #import "XJLaunchManager.h"
 #import "XJRemoteConfigManager.h"
-#import <AFNetworking/AFNetworking.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
--- XJLaunchManager/XJRemoteConfigManager/XJRemoteConfigManagerProtocol.h
+++ XJLaunchManager/XJRemoteConfigManager/XJRemoteConfigManagerProtocol.h
@@ -10,7 +10,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-
 @protocol XJRemoteConfigManagerProtocol <NSObject>
 
 @optional
